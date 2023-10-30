74e23a764d03318c60a7f0577f6f9bef74a84629
F1ner <123871318+F1ner@users.noreply.github.com>
2023-10-30 15:27:54 +0200

    123

---
+++ Компьютерный практикум № 2 (передача параметров)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум___2__передача_параметров_
+{
+    class Program
+    {
+        //Задание 1. Написать метод, «рисующий» на экране горизонтальную линию из 60 символов «*».
+        //static void Main()
+        //{
+        //    DrawLine(60);
+        //    Console.ReadKey();
+        //}
+        //static void DrawLine(int Length)
+        //{
+        //    for(int i=0;i<Length;i++)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //    Console.WriteLine();
+        //}
+        //Задание 2. Написать метод, «рисующий» прямоугольный треугольник указанного вида:
+        //static void Main()
+        //{
+        //    DrawRightTriangle(30);
+        //    Console.ReadKey();
+        //}
+        //static void DrawRightTriangle(int height)
+        //{
+        //    for(int i=1;i<=height;i++)
+        //    {
+        //        for(int k=1;k<=i;k++)
+        //        {
+        //            Console.Write("*");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+        //Задание 3. Вычислить значение выражения, определив и использовав дополнительный метод:
+        //static void Main()
+        //{
+
+
+        //    Console.Write($"x={Algorytm()}");
+        //    Console.ReadKey();
+        //}
+        //public static double Algorytm()
+        //{
+        //    return (Math.Sqrt(5) + 5) / 2 + (Math.Sqrt(12) + 12) / 2 + (Math.Sqrt(19) + 19) / 2;
+        //}
+        //Задание 4. Нахождение максимальной величины из двух целых переменных a, b.
+        //public static void Main()
+        //{
+        //    Console.Write("a=");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.Write("b=");
+        //    int b = int.Parse(Console.ReadLine());
+        //    int max = Max(a, b);
+        //    Console.WriteLine($"Наибольшее число-{max}");
+        //    Console.ReadKey();
+        //}
+        //public static int Max(int a, int b)
+        //{
+        //    if (a > b)
+        //        return a;
+        //    else
+        //        return b;
+        //}
+
+    }
+}
